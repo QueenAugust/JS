ffba6055d6209599e67ac6685802107ef1558bea
QueenAugust <66312016+QueenAugust@users.noreply.github.com>
2020-06-06 10:26:50 +0200

    first try

---
+++ java.js
@@ -0,0 +1,42 @@
+let post = {
+    colors: ['red','blue','green','pink']
+   };
+console.log(post.colors)
+
+post.hasManyColors=""
+
+
+if (colors > 3) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+
+let myfirstboolean=true
+
+console.log(post.colors.lenght)
+
+        // if ([colors]>3) {
+        //     console.log("true")} else {
+        //     console.log("false")
+        //     }
+//if(hasManyColors) {
+    // Do something if the comparison or 
+    //boolean is true
+  //} else {
+    // Do something if it is false}
+  
+  //post.hasManyColors=(if(colors>3) {
+    //console.log("true"); else {
+      //  console.log("false")}})
+//Its value should be true if there are more 
+//colors than 3, otherwise it is expected to 
+//be false
+
+
+console.log(post)
+
+let finish = "This is the last line"
+console.log(finish)
+
+
